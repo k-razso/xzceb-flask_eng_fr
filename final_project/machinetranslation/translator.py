3b5896bb7fa8c65e1333a839809cf3c4fa72acfc
k-razso <konstantin.razso@gmail.com>
2022-11-14 00:53:32 -0500

    Temp changes2

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
-import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
+#import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,22 +17,26 @@ language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
-language_translator.set_service_url('https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/1e35511b-e05f-4d98-98c4-05aa4a2ac83b')
+language_translator.set_service_url(
+    'https://api.eu-de.language-translator.watson.\
+     cloud.ibm.com/instances/1e35511b-e05f-4d98-98c4-05aa4a2ac83b')
 
 
 def englishToFrench(englishText):
     #write the code here
-    translation = language_translator.translate(
+    translated_text = language_translator.translate(
         text=englishText,
         model_id='en-fr').get_result()
-    return translation["translations"[0]"translation"]
+    french = translated_text["translations"][0]["translation"]
+    return french
 
 
 def frenchToEnglish(frenchText):
     #write the code here
-    translatedText = language_translator.translate(
+    translated_text = language_translator.translate(
         text=frenchText,
         model_id='fr-en').get_result()
-    return translation["translations"[0]"translation"]
+    english = translated_text["translations"][0]["translation"]
+    return english
 
-englishToFrench(Hello)
+
