5e1c11e6490337fa2b9f093665e28085d199ad98
k-razso <konstantin.razso@gmail.com>
2022-11-14 02:30:42 -0500

    temp changes 3

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,26 +1,26 @@
 import unittest
 
-from translator.py import frenchToEnglish, englishToFrench
+from translator.py import french_to_english, english_to_french
 
-class TestEngToFrench(unittest.TestCase)
+class TestEngToFrench(unittest.TestCase):
     def testEng2Fr(self):
         # Write at least 2 tests
-        self.assertEqual(englishToFrench,'man','homme')
-        self.assertEqual(englishToFrench,'bread','pain')
+        self.assertEqual(english_to_french('man'),'homme')
+        self.assertEqual(english_to_french('bread'),'pain')
         # Test for the translation of the world ‘Hello’ and ‘Bonjour’
-        self.assertEqual(englishToFrench,'Hello','Bonjour')
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
         # Testing for null input
-        self.assertEqual(englishToFrench,'','')
+        #self.assertEqual(english_to_french,'','')
     
- class TestFrenchToEng(unittest.TestCase)   
+class TestFrenchToEng(unittest.TestCase):
     def testFr2Eng(self):
         # Write at least 2 tests
-        self.assertEqual(frenchToEnglish,'homme','man')
-        self.assertEqual(frenchToEnglish,'pain','bread')
+        self.assertEqual(french_to_english('homme'),'man')
+        self.assertEqual(french_to_english('pain'),'bread')
         # Test for the translation of the world ‘Hello’ and ‘Bonjour’
-        self.assertEqual(frenchToEnglish,'Bonjour','Hello')
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
         # Testing for null input
-        self.assertEqual(frenchToEnglish,'','')
+        #self.assertEqual(french_to_english,'','')
 
-if __name__==if __name__ == "__main__":
+if __name__ == "__main__":
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,42 +1,40 @@
+"""this module implements an instance of IBM language translator"""
 import os
+import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-#import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+APIKEY = os.environ['apikey']
+URL = os.environ['url']
 
-# Following code follows cloud.ibm.com language translator API documentation
-# Creates an instance of IBM Watson language translator with personal credentials
+""" Following code follows cloud.ibm.com language translator API documentation
+ Creates an instance of IBM Watson language translator with personal credentials """
 
-authenticator = IAMAuthenticator('SRE83uTbWF7bFCrHeoOtlnbc1Mdtgjuhk-jcTSNLJ40W')
-language_translator = LanguageTranslatorV3(
+AUTHENTICATOR = IAMAuthenticator(APIKEY)
+LANGUAGE_TRANSLATOR = LanguageTranslatorV3(
     version='2018-05-01',
-    authenticator=authenticator
+    authenticator=AUTHENTICATOR
 )
-language_translator.set_service_url(
-    'https://api.eu-de.language-translator.watson.\
-     cloud.ibm.com/instances/1e35511b-e05f-4d98-98c4-05aa4a2ac83b')
+
+LANGUAGE_TRANSLATOR.set_service_url(URL)
 
 
-def englishToFrench(englishText):
-    #write the code here
-    translated_text = language_translator.translate(
-        text=englishText,
+def english_to_french(english_text):
+    """ Function defining a way to translate a given english text into french"""
+    translated_text = LANGUAGE_TRANSLATOR.translate(
+        text=english_text,
         model_id='en-fr').get_result()
-    french = translated_text["translations"][0]["translation"]
-    return french
+    french_text = translated_text["translations"][0]["translation"]
+    return french_text
 
 
-def frenchToEnglish(frenchText):
-    #write the code here
-    translated_text = language_translator.translate(
-        text=frenchText,
+def french_to_english(french_text):
+    """ Function defining a way to translate a given french text into english"""
+    translated_text = LANGUAGE_TRANSLATOR.translate(
+        text=french_text,
         model_id='fr-en').get_result()
-    english = translated_text["translations"][0]["translation"]
-    return english
-
-
+    english_text = translated_text["translations"][0]["translation"]
+    return english_text
